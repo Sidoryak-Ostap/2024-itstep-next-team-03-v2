10f0c7da4800d86276d3eb833ec78c403570e970
Sidoryak-Ostap <ostapsidorak@gmail.com>
2024-11-07 00:06:59 +0200

    fixes v-3

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -4,13 +4,13 @@ export const products: ProductItem[] = [
   {
     id: 1,
     productName: 'Gold earrings "Congo"',
-    category: "earrings",
+    categoryId: 2,
     article: "12486172205",
     manufacturer: "Gucci",
     price: 9800,
     currency: "₴",
     quantityAvailable: 18,
-    clasps: ["Clasp"],
+    clasps: "Clasp",
     attributes: [
       {
         attributeName: "Metal",
@@ -23,5 +23,6 @@ export const products: ProductItem[] = [
     ],
     description:
       "A sophisticated accessory in the style of classicism that will complement your image Made of high-quality gold, these earrings have a shiny surface that emphasizes their luxury. The spiral shape adds uniqueness and originality to these rings. They look modern and classic at the same time, so they are ideal for both everyday wear and special events. With a diameter of 50 mm, these earrings clearly emphasize your individuality without being too massive. Thanks to the reliable lock, the earrings are comfortable to wear, and they will not fall out during active pastime.",
+    imageUrl: "dsa",
   },
 ];
